cc9ac8b0019fdeaba54028bbd2b9c7c1c4bf4ebc
shantokarmoker8 <shantokarmoker8@gmail.com>
2026-04-03 14:00:43 +0600

    icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,9 +37,6 @@ export default function Navbar() {
             <li>
               <a href="#contact">Contact</a>
             </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
           </ul>
         </div>
         <a href="https://ars-soft-solutions.vercel.app/">
